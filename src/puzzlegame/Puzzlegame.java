71c8bbb1846bbcf426fc7224f2e03f29fc014155
Mikael Fredriksson <224Mikael@gmail.com>
2018-10-31 16:01:12 +0100

    Fungerande som helhet. Småfixar skall göras!

--- src/puzzlegame/Puzzlegame.java
+++ src/puzzlegame/Puzzlegame.java
@@ -1,23 +1,20 @@
 package puzzlegame;
 
 import java.awt.BorderLayout;
-import java.awt.Font;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
-import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
-public class Puzzlegame extends JFrame implements ActionListener {
+public class Puzzlegame extends JFrame {
 
     private JPanel p = new JPanel();
     private JPanel k = new JPanel();
@@ -25,12 +22,6 @@ public class Puzzlegame extends JFrame implements ActionListener {
     
     private JButton nyttSpel = new JButton("Nytt Spel");
     private JButton sluta = new JButton("Avsluta");
-
-    // Skapar två listor för att ändra storleken på spelfältet
-    String[] kartBreddBox = { "3", "4", "5", "7", "10" };
-    String[] kartHöjdBox = { "3", "4", "5", "7", "10" };
-    JComboBox kartBreddLista = new JComboBox(kartBreddBox);
-    JComboBox kartHöjdLista = new JComboBox(kartHöjdBox);
         
     int kartBredd = 4;
     int kartHöjd = 4;
@@ -39,152 +30,110 @@ public class Puzzlegame extends JFrame implements ActionListener {
     List<JButton> buttons = new ArrayList<>();
 
     public Puzzlegame() {
-
+        Handler handler = new Handler(); 
+        
         int tal = 0;
-
-        for (int y = 0; y < kartHöjd; y++) {
-            for (int x = 0; x < kartBredd; x++) //kan ju här även skapa knapparna vilket även gör den delen dynamisk
-            {
-                if (tal == 0)
-                    buttons.add(new JButton("-"));
-                else 
-                    buttons.add(new JButton(tal + ""));
-                
-                Collections.shuffle(buttons);
-                spelplan[x][y] = buttons.get(tal);
-                buttons.get(tal).addActionListener(this); //??
-                k.add(buttons.get(tal));
-                tal++;
-                if (spelplan[x][y].getText() == "-") {
-                    System.out.println("success");
-                }
-            }
-        }
-
-        tal = 0;
-        //byggarbetsplats
+        
         for (int y = 0; y < kartHöjd; y++) //egentligen behövs bara en längd/höjd då det är en kvadrat
         {
             for (int x = 0; x < kartBredd; x++) //kan ju här även skapa knapparna vilket även gör den delen dynamisk
             {
+                if (tal == (kartBredd * kartHöjd - 1)) {
+                    buttons.add(new JButton(" ")); 
+                }
+                else 
+                    buttons.add(new JButton((tal + 1) + ""));
+                
                 spelplan[x][y] = buttons.get(tal);
-                buttons.get(tal).addActionListener(this); //??
+                buttons.get(tal).addActionListener(handler);
                 k.add(buttons.get(tal));
                 tal++;
-                if (spelplan[x][y].getText() == "-") {
-                    System.out.println("success");
-                }
             }
         }
-
-        System.out.println(spelplan[2][2].getClass().getName()); //vi vil dock kunan fp fram namn
-        System.out.println(spelplan[2][3].getText());
-        System.out.println(spelplan[2][3].getActionCommand());
-        System.out.println(spelplan[0][0].getText());
-    //area.setFont(new Font("Monospaced", Font.PLAIN, 12));
-        //p.setLayout(new GridLayout(1,6)); // y, x i detta fall, försöker jämna fördelning
-        //p.add(new JLabel("Filnamn: ", JLabel.CENTER)); //bestämmer placering av text, höger/vänster/center osv
-        // samt skapar textrutan genom JLabel
+        
+        shuffle(spelplan);
+        
         p.add(nyttSpel);
         p.add(sluta);
         k.setLayout(new GridLayout(kartHöjd, kartBredd)); // (y,x) dock av ngn anleding
         
-        b.add(kartBreddLista);
-        b.add(kartHöjdLista);
-        
         add(p, BorderLayout.NORTH); //denna ger oss platsen "högst upp" på vår panel
         add(k, BorderLayout.CENTER);
-        add(b, BorderLayout.SOUTH);
 
-        sluta.addActionListener(this);
-        nyttSpel.addActionListener(this);
-        
-        kartBreddLista.setSelectedIndex(1); // Anger vilket alternativ som är aktiverat från start på listorna
-        kartHöjdLista.setSelectedIndex(1);
-        
-        //kartBreddLista.addActionListener(this); Dessa skall användas för att skapa funktionalitet på listorna
-        //kartHöjdLista.addActionListener(this);
+        sluta.addActionListener(handler);
+        nyttSpel.addActionListener(handler);
 
-        pack();
+        setSize(380, 400);
+        setResizable(false);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
+    
+    private class Handler implements ActionListener {
+        
+        @Override
+        public void actionPerformed(ActionEvent e) {
 
-        JButton clickedButton = (JButton) e.getSource();
+            JButton clickedButton = (JButton) e.getSource();
 
-        // undersök vilken knapp användaren har tryckt på
-        int tal = 0;
-        for (int y = 0; y < kartHöjd; y++) {
+            // undersök vilken knapp användaren har tryckt på
+            for (int y = 0; y < kartHöjd; y++) {
 
-            for (int x = 0; x < kartBredd; x++) {
-                if (spelplan[x][y].equals(clickedButton)) {
-                    if (spelplan[x][y].getText().equals("-")) {
-                        return;
-                    }
-                    // gör att inget händer vid tryck på vår blanka knapp
-                    System.out.println(clickedButton.getText());
+                for (int x = 0; x < kartBredd; x++) {
+                    if (spelplan[x][y].equals(clickedButton)) {
+                        if (x > 0) // kan kolla åt vänster
+                            if (spelplan[x - 1][y].getText().equals(" "))//Blank är till vänster                            
+                                changePos(x, y, x-1, y);
 
-                    if (x > 0) // kan kolla åt vänster
-                    {
-                        if (spelplan[x - 1][y].getText().equals("-")) {
-                            System.out.println("blank är till vänster");
+                        if (x < kartBredd - 1) // kan kolla åt höger
+                            if (spelplan[x + 1][y].getText().equals(" "))//Blank är till höger                            
+                                changePos(x, y, x+1, y);
 
-                            String tempBtnName = spelplan[x][y].getText();
-                            spelplan[x][y].setText(spelplan[x - 1][y].getText());
-                            spelplan[x - 1][y].setText(tempBtnName);
-                        }
-                    }
-                    if (x < kartBredd - 1) // kan kolla åt höger
-                    {
-                        if (spelplan[x + 1][y].getText().equals("-")) {
-                            System.out.println("blank är till höger");
+                        if (y > 0) // kan kolla uppåt
+                            if (spelplan[x][y - 1].getText().equals(" ")) //Blank är ovanför
+                                changePos(x, y, x, y-1);
 
-                            String tempBtnName = spelplan[x][y].getText();
-                            spelplan[x][y].setText(spelplan[x + 1][y].getText());
-                            spelplan[x + 1][y].setText(tempBtnName);
-                        }
-                    }
-                    if (y > 0) // kan kolla uppåt
-                    {
-                        if (spelplan[x][y - 1].getText().equals("-")) {
-                            System.out.println("blank är ovanför");
-
-                            String tempBtnName = spelplan[x][y].getText();
-                            spelplan[x][y].setText(spelplan[x][y - 1].getText());
-                            spelplan[x][y - 1].setText(tempBtnName);
-                        }
-                    }
-                    if (y < kartHöjd - 1) // kan kolla nedåt
-                    {
-                        if (spelplan[x][y + 1].getText().equals("-")) {
-                            System.out.println("blank är nedanför");
-
-                            String tempBtnName = spelplan[x][y].getText();
-                            spelplan[x][y].setText(spelplan[x][y + 1].getText());
-                            spelplan[x][y + 1].setText(tempBtnName);
-                        }
+                        if (y < kartHöjd - 1) // kan kolla nedåt
+                            if (spelplan[x][y + 1].getText().equals(" ")) //Blank är nedanför                            
+                                changePos(x, y, x, y+1);
                     }
                 }
             }
-        }
 
-        if (e.getSource() == sluta) {
-            System.exit(0);
-        }
-        if (e.getSource() == nyttSpel) {
-            shuffle(spelplan);
+            if (e.getSource() == sluta) {
+                System.exit(0);
+            }
+            if (e.getSource() == nyttSpel) {
+                shuffle(spelplan);
+            }
         }
     }
+    
+    public void changePos(int x1, int y1, int x2, int y2) {
+        String tempBtnName = spelplan[x1][y1].getText();
+        spelplan[x1][y1].setText(spelplan[x2][y2].getText());
+        spelplan[x2][y2].setText(tempBtnName);
+        
+        checkIfGameWon();
+    }
 
-    public void gameOver(JButton[][] a) {
+    public void checkIfGameWon() {
+        int tal = 0;
+        int score = 0;
         for (int y = 0; y < kartHöjd; y++) {
             for (int x = 0; x < kartBredd; x++) //kan ju här även skapa knapparna vilket även gör den delen dynamisk
             {
-                //spelplan[x][y] = buttons.get(tal);
+                if (tal == (kartHöjd * kartBredd - 1))
+                    if (spelplan[x][y].getText().equals(" "))
+                        score++;
+                
+                if (spelplan[x][y].getText().equals((tal + 1) + ""))
+                    score++;
+                
+                tal++;
+                if (score == 16)
+                    JOptionPane.showMessageDialog(null, "Grattis, du vann!");
             }
         }
     }
@@ -211,3 +160,9 @@ public class Puzzlegame extends JFrame implements ActionListener {
     }
 
 }
+
+/*
+    Lägga till antalet omstarter
+    Lägga till antalet flytt
+    Lägga till bakgrundsmusik?
+*/
